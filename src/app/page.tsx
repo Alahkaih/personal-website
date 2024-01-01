d37e7ab0d752de6a248d3df269c79f243e1e2d5f
Alahkaih Andrus <alahkaihandrus@gmail.com>
2023-12-31 23:36:07 -0800

    changed colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image"
+import {tex} from '@mui/material'
 export default function Home() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <p>
-        <span className="text-red-500">{" This is Alahkaih's personal website."}</span>
-        <span className="text-green-500">{" It is currently a work in progress. "}</span>
-        <span className="text-blue-500">{" Check back later"}</span>
-        <span className="text-yellow-500">{" for updates."}</span>
+        <span className="text-black">{" This is Alahkaih's personal website."}</span>
+        <span className="text-black">{" It is currently a work in progress. "}</span>
+        <span className="text-black">{" Check back later"}</span>
+        <span className="text-black">{" for updates."}</span>
       </p>
       <Image src={"/funnyRaccoon.jpg"} alt="Raccoon" width={500} height={500}/>
     </div>
